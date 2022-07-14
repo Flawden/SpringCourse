0a6585d4797c9ce4f582a8daba101acaa1b079e9
Flawden <43092499+Flawden@users.noreply.github.com>
2022-07-14 13:21:39 +0300

    scope - prototype

--- src/main/java/ru/flawden/springcourse/TestSpring.java
+++ src/main/java/ru/flawden/springcourse/TestSpring.java
@@ -7,8 +7,11 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         MusicPlayer music = context.getBean("MusicPlayer", MusicPlayer.class);
+        MusicPlayer music1 = context.getBean("MusicPlayer", MusicPlayer.class);
+        music.setName("Lavash");
         music.playMusic();
-
+        System.out.println();
+        music1.playMusic();
         context.close();
     }
 }
