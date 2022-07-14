8de45ec59e9f871a7c992eb913c9d488b16e6791
Flawden <43092499+Flawden@users.noreply.github.com>
2022-07-14 11:53:40 +0300

    First homework

--- src/main/java/ru/flawden/springcourse/MusicPlayer.java
+++ src/main/java/ru/flawden/springcourse/MusicPlayer.java
@@ -2,24 +2,27 @@ package ru.flawden.springcourse;
 
 import ru.flawden.springcourse.Music.Music;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> music = new ArrayList<>();
 
     private String name;
     private int volume;
 
     public MusicPlayer(){}
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
 
-    public Music getMusic() {
+    public List<Music> getMusic() {
         return music;
     }
 
-    public void setMusic(Music music) {
+    public void setMusic(List<Music> music) {
         this.music = music;
     }
 
@@ -40,8 +43,8 @@ public class MusicPlayer {
     }
 
     public void playMusic() {
-        System.out.println(name + "\n" +
-                "Playing: " + music.getSong() + "\n" +
-                "Volume: " + volume + "%");
+        System.out.println(name);
+        music.forEach(music -> System.out.println("Playing: " + music.getSong()));
+        System.out.println("Volume: " + volume + "%");
     }
 }
