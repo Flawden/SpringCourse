f4cf8552ea7168ba1b93d461a2605bf599a3aca3
Flawden <43092499+Flawden@users.noreply.github.com>
2022-07-18 18:45:06 +0300

    Data is received via a Get request

---
+++ src/main/java/ru/flawden/springcourse/controller/FirstController.java
@@ -0,0 +1,37 @@
+package ru.flawden.springcourse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "first/hello";
+    }
+
+    @GetMapping("/welcome")
+    public String sayHello(HttpServletRequest request) {
+        System.out.println(request.getParameter("name"));
+        return "first/hello";
+    }
+
+    @GetMapping("/welcome-alter")
+    public String sayHello(@RequestParam(required = false, name = "name") String name) {
+        System.out.println(name);
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage() {
+        return "first/goodbye";
+    }
+
+}
