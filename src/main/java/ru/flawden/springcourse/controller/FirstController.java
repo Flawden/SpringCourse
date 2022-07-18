c1c91fb1283a17b8f4bc3c6ca0aa0442470c2104
Flawden <43092499+Flawden@users.noreply.github.com>
2022-07-18 19:08:47 +0300

    Homework 4: Basic Calculator

--- src/main/java/ru/flawden/springcourse/controller/FirstController.java
+++ src/main/java/ru/flawden/springcourse/controller/FirstController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/first")
@@ -27,6 +28,27 @@ public class FirstController {
     public String sayHello(@RequestParam(required = false, name = "name") String name, Model model) {
         model.addAttribute("name", name);
         return "first/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(name = "a") Double a, @RequestParam(name = "b") Double b, @RequestParam(name = "action") String action, Model model) {
+        if (action.equalsIgnoreCase("addition")) {
+            model.addAttribute("result", a + b);
+        } else if (action.equalsIgnoreCase("subtraction")) {
+            model.addAttribute("result", a - b);
+        } else if (action.equalsIgnoreCase("division")) {
+            model.addAttribute("result", a / b);
+        } else if (action.equalsIgnoreCase("multiplication")) {
+            model.addAttribute("result", a * b);
+        } else {
+            model.addAttribute("result", "Incorrect value for one of the fields.\n" +
+                    "\n" +
+                    "The input must contain:\n" +
+                    "a - number\n" +
+                    "b - number\n" +
+                    "action - action (addition, subtraction, division, multiplication)");
+        }
+        return "first/calculator";
     }
 
     @GetMapping("/goodbye")
