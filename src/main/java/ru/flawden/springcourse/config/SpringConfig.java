207ac199e5856ff2415fd1c61d8e5bcbcd2fc1b0
Flawden <43092499+Flawden@users.noreply.github.com>
2022-07-21 15:08:04 +0300

    JDBC template: part one

--- src/main/java/ru/flawden/springcourse/config/SpringConfig.java
+++ src/main/java/ru/flawden/springcourse/config/SpringConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
+import java.sql.DriverManager;
+import java.sql.JDBCType;
 
 @Configuration
 @ComponentScan("ru.flawden.springcourse")
@@ -39,11 +45,25 @@ public class SpringConfig implements WebMvcConfigurer {
         return templateEngine;
     }
 
-
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/SpringCourse");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("password");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
--- src/main/java/ru/flawden/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/flawden/springcourse/dao/PersonDAO.java
@@ -1,5 +1,6 @@
 package ru.flawden.springcourse.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.flawden.springcourse.models.Person;
 
@@ -9,6 +10,8 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
+
+    private JdbcTemplate jdbcTemplate;
 
     private static final String URL = "jdbc:postgresql://localhost:5432/SpringCourse";
     private static final String username = "postgres";
@@ -29,6 +32,10 @@ public class PersonDAO {
         }
     }
 
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void createPerson(Person person) {
         try {
             PreparedStatement statement = connection.prepareStatement("INSERT INTO Person(firstname, surname, age, email) VALUES(?, ?, ?, ?)");
@@ -43,52 +50,14 @@ public class PersonDAO {
     }
 
     public Person show(int id) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Person WHERE id = ?");
-            statement.setInt(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if(resultSet.next()) {
-                Person person = new Person();
-                person.setId(resultSet.getInt("id"));
-                person.setFirstname(resultSet.getString("firstname"));
-                person.setSurname(resultSet.getString("surname"));
-                person.setAge(resultSet.getInt("age"));
-                person.setEmail(resultSet.getString("email"));
-                return person;
-            }
-            return null;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?",new Object[]{id}, new PersonMapper()).stream().findAny().orElse(null);
     }
 
     public List<Person> peopleList() {
-        List<Person> people = new ArrayList<>();
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Person");
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                Person person = new Person();
-                person.setId(resultSet.getInt("id"));
-                person.setFirstname(resultSet.getString("firstname"));
-                person.setSurname(resultSet.getString("surname"));
-                person.setAge(resultSet.getInt("age"));
-                person.setEmail(resultSet.getString("email"));
-                people.add(person);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return people;
+        return jdbcTemplate.query("SELECT * FROM Person", new PersonMapper());
     }
 
     public void updatePerson(Person person, Integer id) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setEmail(person.getEmail());
-        personToBeUpdated.setFirstname(person.getFirstname());
-        personToBeUpdated.setSurname(person.getSurname());
-        personToBeUpdated.setAge(person.getAge());
         try {
             PreparedStatement statement = connection.prepareStatement("UPDATE Person " +
                     "SET firstname = ?," +
@@ -96,10 +65,10 @@ public class PersonDAO {
                     "age = ?," +
                     "email = ?" +
                     "WHERE id = ?");
-            statement.setString(1, personToBeUpdated.getFirstname());
-            statement.setString(2, personToBeUpdated.getSurname());
-            statement.setInt(3, personToBeUpdated.getAge());
-            statement.setString(4, personToBeUpdated.getEmail());
+            statement.setString(1, person.getFirstname());
+            statement.setString(2, person.getSurname());
+            statement.setInt(3, person.getAge());
+            statement.setString(4, person.getEmail());
             statement.setInt(5, id);
             statement.executeUpdate();
         } catch (SQLException e) {
