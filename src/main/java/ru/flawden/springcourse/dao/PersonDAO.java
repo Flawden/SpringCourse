3d8b50a14af2c7d669ccdceaed11c800fe9106db
Flawden <43092499+Flawden@users.noreply.github.com>
2022-07-22 13:17:02 +0300

    Batch update VS Ordinary update database

--- src/main/java/ru/flawden/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/flawden/springcourse/dao/PersonDAO.java
@@ -1,5 +1,6 @@
 package ru.flawden.springcourse.dao;
 
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -36,5 +37,50 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Person WHERE id = ?", id);
+    }
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000people();
+        long before = System.currentTimeMillis();
+
+        for (Person person: people) {
+            jdbcTemplate.update("INSERT INTO Person(firstname, surname, age, email) VALUES(?, ?, ?, ?)",
+                    person.getFirstname(), person.getSurname(), person.getAge(), person.getEmail());
+        }
+
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after - before));
+    }
+
+    private List<Person> create1000people() {
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person("Firstname" + i, "Surname" + i, "Email" + i + "@Mail.ru", 30));
+        }
+        return people;
+    }
+
+    public void testBatchUpdate() {
+
+        List<Person> people = create1000people();
+        long before = System.currentTimeMillis();
+        jdbcTemplate.batchUpdate("INSERT INTO Person(firstname, surname, age, email) VALUES(?, ?, ?, ?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, people.get(i).getFirstname());
+                ps.setString(2, people.get(i).getSurname());
+                ps.setInt(3, people.get(i).getAge());
+                ps.setString(4, people.get(i).getEmail());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return people.size();
+            }
+        });
+
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after - before));
+
     }
 }
